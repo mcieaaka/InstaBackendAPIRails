25b1e03f2c48c40e5ffd1fc74c1465b7bd9f506a
Harshit Srivastava <harshit0300@gmail.com>
2022-08-10 00:59:32 +0530

    Fix for Helper methods

--- app/api/helper_methods/helpers.rb
+++ app/api/helper_methods/helpers.rb
@@ -1,33 +1,33 @@
-# module HelperMethods
-#     class Helpers
-#         extend Grape::API::Helpers
+module HelperMethods
+    module Helpers
+        extend Grape::API::Helpers
         
-#         # config.middleware.use ActionDispatch::Session::CookieStore
-#         use ActionDispatch::Session::CookieStore
-#         # helpers do
-#         def session
-#             env['rack.session']
-#         end
+        # config.middleware.use ActionDispatch::Session::CookieStore
+        # use ActionDispatch::Session::CookieStore
+        # helpers do
+        def session
+            env['rack.session']
+        end
 
-#         def login_in (user)
-#             session[:user_id] = user.id
-#         end
+        def login_in (user)
+            session[:user_id] = user.id
+        end
 
-#         def current_user
-#             if session[:user_id]
-#                 @current_user ||= User.find_by(id: session[:user_id])
-#             end
-#         end
+        def current_user
+            if session[:user_id]
+                @current_user ||= User.find_by(id: session[:user_id])
+            end
+        end
 
-#         def logged_in?
-#             !current_user.nil?
-#         end
+        def logged_in?
+            !current_user.nil?
+        end
 
-#         def log_out
-#             session.delete(:user_id)
-#             # reset_session
-#             @current_user = nil
-#         end
-#     end
-#     # end
-# end
+        def log_out
+            session.delete(:user_id)
+            # reset_session
+            @current_user = nil
+        end
+    end
+    # end
+end
--- app/api/insta_post/comment_api.rb
+++ app/api/insta_post/comment_api.rb
@@ -1,30 +1,6 @@
 module InstaPost
     class CommentAPI < Grape::API
-        helpers do
-            def session
-                env['rack.session']
-            end
-
-            def login_in (user)
-                session[:user_id] = user.id
-            end
-
-            def current_user
-                if session[:user_id]
-                    @current_user ||= User.find_by(id: session[:user_id])
-                end
-            end
-
-            def logged_in?
-                !current_user.nil?
-            end
-
-            def log_out
-                session.delete(:user_id)
-                # reset_session
-                @current_user = nil
-            end
-        end
+        helpers HelperMethods::Helpers
 
         use ActionDispatch::Session::CookieStore
         version 'v1',using: :path
